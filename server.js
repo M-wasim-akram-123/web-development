cdf626fa1668cd97a59baa2b9017d63421b33760
wasim.akram <wasim.akram@techletspk.com>
2024-04-25 10:41:26 +0500

    added express and basic api path explanation

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 // Import required modules
 const http = require('http');
+// Import required modules
+const express = require('express');
 
+// Create an Express application
+const app = express();
 // Define port number
 const PORT = process.env.PORT || 3000;
 // Create HTTP server
-const server = http.createServer((req, res) => {
-  // Set response headers
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+const server = http.Server(app);
+app.get('/helloWorld',(req,res)=>{
+ res.send("hello world")
+})
+app.get('/',(req,res)=>{
+  res.send("base path")
+ })
+ app.get('/test',(req,res)=>{
 
-  // Send response
-  res.end('Hello, World!\n');
-});
 
+  
+  res.send("base path test url")
+ })
 // Start the server
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
